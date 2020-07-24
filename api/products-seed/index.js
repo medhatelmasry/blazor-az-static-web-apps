31caa3247134d000cceed681b37192c1726edb8c
Medhat Elmasry <medhat@elmasry.ca>
2020-07-24 13:04:41 -0700

    1st commit

---
+++ api/products-seed/index.js
@@ -0,0 +1,19 @@
+const data = require('../shared/product-data');
+
+
+module.exports = async function (context, req) {
+
+  try {
+
+    const result = await data.seedProducts();
+
+    context.res.status(200).json(result);
+
+  } catch (error) {
+
+    context.res.status(500).send(error);
+
+  }
+
+};
+
